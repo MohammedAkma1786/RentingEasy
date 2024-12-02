134b01547305b107bd84f77d1bc36c81557f5920
Mohammed <mohammedakmal003@gmail.com>
2024-12-02 13:51:17 +0000

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,28 +6,60 @@ import CreateListing from "./pages/CreateListing";
 import ListingDetails from "./pages/ListingDetails";
 import TripList from "./pages/TripList";
 import WishList from "./pages/WishList";
-import PropertyList from "./pages/propertyList";
+import PropertyList from "./pages/PropertyList"; // Corrected the import path
 import ReservationList from "./pages/ReservationList";
 import Category from "./pages/Category";
 import Search from "./pages/Search";
-import Header from "./components/Header";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <div className="text-[#404040] bg-primary">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/create-listing" element={<CreateListing />} />
-          <Route path="/listing/:listingId" element={<ListingDetails />} />
-          <Route path="/listing/category/:category" element={<Category />} />
-          <Route path="/listing/search/:search" element={<Search />} />
-          <Route path="/:userId/trips" element={<TripList />} />
-          <Route path="/:userId/wishlist" element={<WishList />} />
-          <Route path="/:userId/listing" element={<PropertyList />} />
-          <Route path="/:userId/reservations" element={<ReservationList />} />
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route
+            path="/register"
+            element={<Register />}
+          />
+          <Route
+            path="/create-listing"
+            element={<CreateListing />}
+          />
+          <Route
+            path="/listing/:listingId"
+            element={<ListingDetails />}
+          />
+          <Route
+            path="/listing/category/:category"
+            element={<Category />}
+          />
+          <Route
+            path="/listing/search/:search"
+            element={<Search />}
+          />
+          <Route
+            path="/:userId/trips"
+            element={<TripList />}
+          />
+          <Route
+            path="/:userId/wishlist"
+            element={<WishList />}
+          />
+          <Route
+            path="/:userId/listing"
+            element={<PropertyList />}
+          />
+          <Route
+            path="/:userId/reservations"
+            element={<ReservationList />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
