bb72da9ed1c18dc613d1b6fd40c5deaaa8f2e932
Mohammed <mohammedakmal003@gmail.com>
2024-12-17 08:12:11 +0000

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".", // Root directory is the current directory
   build: {
-    outDir: "dist", // Output directory for build files
+    outDir: "dist",
     rollupOptions: {
-      input: "index.html", // Specify the entry point in the root directory
+      input: "/index.html", // Specify the entry point
     },
   },
 });
